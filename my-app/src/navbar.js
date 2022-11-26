378b9fb8dee0fef5bc967768696b1a122bb6ac6c
lseeker5 <118029799+lseeker5@users.noreply.github.com>
2022-11-26 21:14:22 +0800

    updates

--- my-app/src/navbar.js
+++ my-app/src/navbar.js
@@ -3,12 +3,13 @@ export default function NavBar(){
         <div className="nav-bar">
             <button type="button">Sign In</button>
             <select>
-                <option>Calender</option>
-                <option>Upcoming Community Events</option>
-                <option>Meet-ups</option>
-                <option>Chats</option>
-                <option></option>
-                <option></option>
+                <option value="" disabled selected hidden>What would you like to do?</option>
+                <option value="Calender">Calender</option>
+                <option value="Upcoming Community Events">Upcoming Community Events</option>
+                <option value="Meet-ups">Meet-ups</option>
+                <option value="Chats">Chats</option>
+                <option value="Community Space">Community Space</option>
+                <option value="Settings">Settings</option>
             </select>
         </div>
         
